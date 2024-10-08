88f08db19e85ccc4da7c4da99873ec245a20e6ee
sodonne6 <odonnellsd1@gmail.com>
2024-10-08 18:16:15 +0100

    Update fsm_tb.sv

--- Verilog/Lab2/fsm_tb.sv
+++ Verilog/Lab2/fsm_tb.sv
@@ -1,6 +1,6 @@
 module fsm_tb;
 
-    // Wires to connect FSM, stimulus, and monitor
+    //wires to connect FSM, counter, stimulus generator, and scoreboard monitor
     wire clk, reset;
     wire a, b;
     wire enter, exit;
@@ -8,46 +8,46 @@ module fsm_tb;
     wire [3:0] count_exp;
     wire [3:0] count;
 
-    // Instantiate stimulus generator
+    //stimulus generator
     stim_gen stim_gen (
-        .clk(clk),
-        .reset(reset),
-        .a(a),
-        .b(b),
-        .inc_exp(inc_exp),
-        .dec_exp(dec_exp),
-        .count_exp(count_exp)
+        .clk(clk), //clock signal
+        .reset(reset), //reset 
+        .a(a), //sensor a
+        .b(b), //sensor b
+        .inc_exp(inc_exp), //expected increment signal
+        .dec_exp(dec_exp), //expected decrement signal
+        .count_exp(count_exp) //expected count value
     );
     
     // Instantiate FSM
     fsm uut (
-        .clk(clk),
-        .reset(reset),
-        .a(a),
-        .b(b),
-        .enter(enter),
-        .exit(exit)
+        .clk(clk), //clock signal
+        .reset(reset), //reset signal
+        .a(a), //sensor a 
+        .b(b), //sensor b
+        .enter(enter), //enter flag signal
+        .exit(exit)  //exit flag signal
     );
 
     // Instantiate counter
     counter counter (
-        .clk(clk),
-        .reset(reset),
-        .inc(enter),
-        .dec(exit),
-        .count(count)
+        .clk(clk), //clock signal
+        .reset(reset), //reset
+        .inc(enter), //counter increment connected to enter signal
+        .dec(exit), //counter decrement connected to exit signal
+        .count(count) //count value - amount of cars in carpark at a given time
     );
 
     // Instantiate scoreboard
     scoreboard scoreboard (
-        .clk(clk),
-        .reset(reset),
-        .inc_exp(inc_exp),
-        .dec_exp(dec_exp),
-        .count_exp(count_exp),
-        .count(count),
-        .enter(enter),
-        .exit(exit)
+        .clk(clk), //clock signal
+        .reset(reset), //reset
+        .inc_exp(inc_exp), //connect inc_exp together in order to compare to enter
+        .dec_exp(dec_exp), //connect dec_exp together in order to compare to exit
+        .count_exp(count_exp), //connect count_exp together in order to compare to count
+        .count(count), //count value from counter module
+        .enter(enter), //enter value from FSM
+        .exit(exit)  //exit value from FSM
     );
 
 endmodule
